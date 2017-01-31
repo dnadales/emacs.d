2e0dba4785e55e74f35d06dec39b8740884bfd1c
Damian Nadales <damian.nadales@gmail.com>
2017-01-31 21:10:01 +0100

    Added a color theme.

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-hoogle-imports t)
