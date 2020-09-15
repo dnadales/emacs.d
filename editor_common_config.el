68eb66d54d039114499d05300ee6834651e114d5
Damian Nadales <damian.nadales@iohk.io>
2020-09-15 13:30:29 +0200

    Add a function to move the file associated to the current buffer.

--- editor_common_config.el
+++ editor_common_config.el
@@ -190,5 +190,30 @@ Version 2016-07-17"
 ;; Visually truncate long lines
 (set-default 'truncate-lines t)
 
+;; Source: https://zck.me/emacs-move-file
+(defun move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive (list (expand-file-name
+                      (if buffer-file-name
+                          (read-file-name "Move file to: ")
+                        (read-file-name "Move file to: "
+                                        default-directory
+                                        (expand-file-name (file-name-nondirectory (buffer-name))
+                                                          default-directory))))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (expand-file-name (buffer-file-name))))
+    (message "old file is %s and new file is %s"
+             old-location
+             new-location)
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
+      (delete-file old-location))))
+
+;; Move files in place
+(bind-key "C-x C-m" #'move-file)
+
 (provide 'editor_common_config)
 ;;; editor_common_config.el ends here
