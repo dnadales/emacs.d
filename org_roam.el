4fe459949279651fecfd4e3a76b8742466fecdc0
Damian Nadales <damian.nadales@iohk.io>
2021-08-12 09:47:03 +0200

    Add `org-roam` shortcuts.

--- org_roam.el
+++ org_roam.el
@@ -3,3 +3,10 @@
 (setq org-roam-directory (file-truename "~/Dropbox/docs/org/roam/"))
 
 (org-roam-setup)
+
+;;; Define key bindings for Org-roam
+(global-set-key (kbd "C-c r r") #'org-roam-buffer-toggle)
+(global-set-key (kbd "C-c r b") #'org-roam-buffer)
+(global-set-key (kbd "C-c r i") #'org-roam-node-insert)
+(global-set-key (kbd "C-c r c") #'org-roam-capture)
+(global-set-key (kbd "C-c r /") #'org-roam-node-find)
