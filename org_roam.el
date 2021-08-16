7f90f25a21efffad0f398cb8541cc86fc287d580
Damian Nadales <damian.nadales@iohk.io>
2021-08-16 17:44:54 +0200

    Configure org roam ui and org protocol

--- org_roam.el
+++ org_roam.el
@@ -10,3 +10,22 @@
 (global-set-key (kbd "C-c r i") #'org-roam-node-insert)
 (global-set-key (kbd "C-c r c") #'org-roam-capture)
 (global-set-key (kbd "C-c r /") #'org-roam-node-find)
+
+;; org-roam-ui configuration
+(add-to-list 'load-path "~/.emacs.d/elisp/org-roam-ui")
+(load-library "org-roam-ui")
+(require 'org-roam-protocol)
+
+;; Deft provides an interface for browsing and filtering org-roam notes.
+;;
+;; WARNING: this package can be slow when the number of files becomes large. In
+;; such case you might want to try: https://github.com/hasu/notdeft
+(use-package deft
+  :after org
+  :bind
+  ("C-c r d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory org-roam-directory))
