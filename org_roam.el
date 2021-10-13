4cd62e2bf8b09864364bad8219661991f9e3c173
Damian Nadales <damian.nadales@iohk.io>
2021-10-13 13:15:49 +0200

    Change org roam files directory.

--- org_roam.el
+++ org_roam.el
@@ -1,6 +1,6 @@
 (setq org-roam-v2-ack t)
 (require 'org-roam)
-(setq org-roam-directory (file-truename "~/Dropbox/docs/org/roam/"))
+(setq org-roam-directory (file-truename "~/psys/roam/"))
 
 (org-roam-setup)
 
