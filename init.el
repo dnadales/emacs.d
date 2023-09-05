9e6223e696fe5de01fbdfbcb78a4442fa53e224f
Damian Nadales <damian.only@gmail.com>
2023-09-05 11:39:01 +0200

    Update customize settings

--- init.el
+++ init.el
@@ -56,13 +56,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-export-backends '(ascii html icalendar latex md odt))
+ '(safe-local-variable-values '((haskell-stylish-on-save))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(modus-themes-lang-note ((t (:underline nil))) t)
+ '(modus-themes-lang-warning ((t (:underline nil))) t)
  '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:strike-through nil :foreground "rosy brown"))))
  '(org-level-1 ((t (:foreground "dark turquoise" :height 1.0))))
  '(org-level-2 ((t (:foreground "pale turquoise"))))
