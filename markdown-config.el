6900128d15281822695e10b6485bc4b2db196c8b
Damian Nadales <damian.nadales@iohk.io>
2025-05-28 13:26:18 +0200

    Use visual-line mode in markdown-mode

--- markdown-config.el
+++ markdown-config.el
@@ -8,5 +8,6 @@
 
 (add-hook 'markdown-mode-hook 'visual-fill-column-mode)
 (add-hook 'markdown-mode-hook 'markdown-toggle-markup-hiding)
+(add-hook 'markdown-mode-hook 'visual-line-mode)
 
 ;;; markdown-config.el ends here
