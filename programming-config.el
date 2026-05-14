6e7063faad5e24a7c5ea75d90f63d44225f27043
Damian Nadales <damian.only@gmail.com>
2026-05-14 12:12:48 -0300

    Add envrc for direnv integration
    Automatically sets buffer-local environment from .envrc files so LSP,
    compilation, and vterm pick up per-project nix develop environments.

--- programming-config.el
+++ programming-config.el
@@ -11,4 +11,11 @@
 
 (use-package vterm)
 
+;; direnv integration: automatically set buffer-local environment from
+;; .envrc files so that LSP, compilation, and vterm pick up per-project
+;; nix-shell / nix-develop environments.
+(use-package envrc
+  :config
+  (envrc-global-mode))
+
 ;;; programming-config.el ends here
